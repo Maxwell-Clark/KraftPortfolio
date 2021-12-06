f5f6c391cf8d80981f6058d31e5ae0d019e79e93
Max <maxwell6636@gmail.com>
2021-12-06 00:23:04 -0600

    first commit

---
+++ K_portfolio_site/user_interface/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <template>
+      <b-navbar type="is-light">
+          <template #start>
+             <b-breadcrumb class="ml-3 mt-5" size="is-size-6" type="is-light" align="is-left">
+                <b-breadcrumb-item class="breadcrumb" >Ken Kraft</b-breadcrumb-item>
+                <b-breadcrumb-item class="breadcrumb" >Leadership and Development</b-breadcrumb-item>
+            </b-breadcrumb>
+          </template>
+          <template #end>
+              <b-navbar-item href="#">
+                  Home
+              </b-navbar-item>
+              <b-navbar-item href="#">
+                  Manager Moments
+              </b-navbar-item>
+              <b-navbar-item href="#">
+                  Resume
+              </b-navbar-item>
+              <b-navbar-dropdown label="Info">
+                  <b-navbar-item href="#">
+                      About
+                  </b-navbar-item>
+                  <b-navbar-item href="#">
+                      Contact
+                  </b-navbar-item>
+              </b-navbar-dropdown>
+          </template>
+      </b-navbar>
+    </template>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Home/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld, 
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 5px;
+}
+.breadcrumb a{
+  color: #606060 !important;
+}
+/* .nav_bar {
+  background-color:#F1FAEE !important;
+} */
+
+</style>
---
+++ K_portfolio_site/user_interface/src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+    <div id="main" >
+        <div class="p-4" id="pic_background">
+            <b-image
+                :src="require('@/assets/Profile_image.jpg')"
+                alt="Profile Picture"
+                ratio="601by235"
+                id="picture"
+                :rounded="true"
+                style="width: 12em; height: 12rem"
+                ></b-image>
+            <div class="has-text-black  mt-3" style="margin-top: 10px;">
+                <p class="is-size-1 has-text-weight-bold"> Ken Kraft</p>
+                <br>
+                <p class="is-size-5"> Organizational Leadership and Development </p>
+            </div>
+        </div>
+
+        <div class="has-background-white box p-4">
+            <div class="has-text-dark mt-3" style="margin-top: 10px; width: 10em;">
+                <h1 class="is-size-2 has-text-weight-bold"> Hello!</h1>
+                <br>
+                <p> Here's who I am and what I do </p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;700&display=swap');
+#picture {
+    max-width: 20em;
+    margin-left: 2.5em;
+    margin-bottom: 2.5em;
+}
+#pic_background {
+    border-radius: 5px;
+    width: 20em; 
+    height: 25em; 
+    height: 100%;
+    border-color: transparent;
+    background-color: #b1a7a6;
+    
+}
+#main {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-top: 7em;
+    padding: 10em;
+    /* margin-left: 3em; */
+    font-family: 'Roboto Condensed', sans-serif;
+    background: linear-gradient(90deg, #e5383b 40%, white 5%);
+}
+
+</style>
